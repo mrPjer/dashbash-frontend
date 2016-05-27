786bd96a97ddcc45aede412f087b175f0257091e
Petar Šegina <psegina@ymail.com>
2016-05-27 15:33:05 +0200

    Initialize webgl

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+var gl;
+
+var initWebGL = function(canvas) {
+
+    gl = null;
+
+    try {
+        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    } catch(e) {
+        console.log(e);
+    }
+
+    return gl;
+}
+
 var start = function() {
-	alert("START!");
+    var canvas = document.getElementById("glcanvas");
+
+    gl = initWebGL(canvas);
+
+    if(!gl) {
+        alert("No WebGL support!");
+        return;
+    }
+
+    canvas.onresize = function() {
+        gl.viewport(0, 0, canvas.width, canvas.height);
+    }
+
+    gl.clearColor(1, 0, 0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 }
